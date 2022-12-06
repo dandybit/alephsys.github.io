fc29fa441829e11f05e9696154e29525f46f4b67
dandybit <alejandrohorrilloguzman@hotmail.com>
2022-12-06 19:52:24 +0100

    Remove files for security reasons.

--- covid19_rest_api/utils/mongo_utils.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import urllib.parse
-
-def create_mongo_client():
-    username = urllib.parse.quote_plus('master')
-    password = urllib.parse.quote_plus('thisisjustatest!')
-    client = MongoClient('mongodb://%s:%s@10.211.55.3' % (username, password))
-
-    return client
